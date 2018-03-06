a506e4bc6f76dc17095431a61dc5f3498fa0ba2e
Sithu Aung <sithu_aung@intuit.com>
2018-03-06 11:32:54 -0800

    add log table and Delete logs button

--- src/index.js
+++ src/index.js
@@ -4,14 +4,48 @@ import * as PropTypes from 'prop-types';
 import { createStore, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-const print = console.log;
-
-const reducer = (state=0, action) => {
+/**
+ * The Reducer fun must be a pure function. 
+ * 1. The only data the pure funciton will receive from parameters.
+ * 2. The parameters never be modified without exception.
+ * 3. The pure function cannot cause no side effect.
+ * 4. The only return output must be the actual pure function.
+ * 
+ * When you work with redux, the state is always immutable. You will never mutate the state object.
+ * @param {*} state 
+ * @param {*} action 
+ */
+const reducer = (state={ result: 0, log: [] }, action) => {
     switch (action.type) {
-        case 'ADD': return state + action.value;
-        case 'SUBTRACT': return state - action.value;
-        case 'MULT': return state * action.value;
-        case 'DIV': return state / action.value;
+        case 'ADD': 
+            return {
+                ...state, 
+                result: state.result + action.value,
+                log: state.log.concat({ op: '+', val: action.value }),
+            };
+        case 'SUBTRACT':
+            return {
+                ...state, 
+                result: state.result - action.value,
+                log: state.log.concat({ op: '-', val: action.value }),
+            };
+        case 'MULT':
+            return {
+                ...state, 
+                result: state.result * action.value,
+                log: state.log.concat({ op: '*', val: action.value }),
+            };
+        case 'DIV':
+            return {
+                ...state, 
+                result: state.result / action.value,
+                log: state.log.concat({ op: '/', val: action.value }),
+            };
+        case 'DELETE':
+        return {
+            ...state, 
+            log: [],
+        };
         default: return state;
     }
 };
@@ -22,6 +56,7 @@ const createAddAction = value => ({ type: 'ADD', value });
 const createSubtractAction = value => ({ type: 'SUBTRACT', value });
 const createMultiplyAction = value => ({ type: 'MULT', value });
 const createDivAction = value => ({ type: 'DIV', value });
+const createDeleteAction = () => ({ type: 'DELETE' });
 
 /*
 const bindActionCreators = (actions, dispatch) => {
@@ -82,6 +117,7 @@ class CalculatorTool extends React.Component {
         subtract: PropTypes.func.isRequired,
         multiply: PropTypes.func.isRequired,
         divide: PropTypes.func.isRequired,
+        delete: PropTypes.func.isRequired,
     }
 
     static defaultProps = {
@@ -96,23 +132,48 @@ class CalculatorTool extends React.Component {
 
     render() {
         return (
-            <form>
-                <div>Result: {this.props.result}</div>
-                <div>
-                    <label htmlFor="input">Input:</label>
-                    <input name="input" type="number" defaultValue={0} ref={ input => this.numInput = input } />
-                </div>
-                <button type="button" onClick={ () => this.props.add(Number(this.numInput.value)) }>+</button>
-                <button type="button" onClick={ () => this.props.subtract(Number(this.numInput.value)) }>-</button>
-                <button type="button" onClick={ () => this.props.multiply(Number(this.numInput.value)) }>*</button>
-                <button type="button" onClick={ () => this.props.divide(Number(this.numInput.value)) }>/</button>
-            </form>
+            <div>
+                <form>
+                    <div>Result: {this.props.result}</div>
+                    <div>
+                        <label htmlFor="input">Input:</label>
+                        <input name="input" type="number" defaultValue={0} ref={ input => this.numInput = input } />
+                    </div>
+                    <button type="button" onClick={ () => this.props.add(Number(this.numInput.value)) }>+</button>
+                    <button type="button" onClick={ () => this.props.subtract(Number(this.numInput.value)) }>-</button>
+                    <button type="button" onClick={ () => this.props.multiply(Number(this.numInput.value)) }>*</button>
+                    <button type="button" onClick={ () => this.props.divide(Number(this.numInput.value)) }>/</button>
+                </form>
+                <br />
+                <LogTable log={this.props.log}/>
+                <button type="button" onClick={ () => this.props.delete() }>Clear Logs</button>
+            </div>
+        );
+    }
+}
+
+class LogTable extends React.Component {
+    render() {
+        const rows = this.props.log.map( log => {
+            return <tr key={log}><td>{log.op}</td><td>{log.val}</td></tr>;
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td>Operator</td><td>Value</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         );
     }
 }
 
 const mapStateToProps = state => {
-    return { result: state };
+    return { result: state.result, log: state.log };
 }
 
 const mapDispatchToProps = dispatch => {
@@ -121,6 +182,7 @@ const mapDispatchToProps = dispatch => {
         subtract: createSubtractAction,
         multiply: createMultiplyAction,
         divide: createDivAction,
+        delete: createDeleteAction,
     }, dispatch);
 }
 
